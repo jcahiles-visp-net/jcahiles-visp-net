4001c8af5b7ad5d5d210d24f0536fa95720e711a
unknown <jcahiles@gmail.com>
2017-09-06 00:15:09 +0800

    Added OT, not yet finished

--- app.js
+++ app.js
@@ -29,27 +29,134 @@ var button = document.getElementById('button');
 button.addEventListener('click', function(event){
     wageLabel.innerHTML = 'getting account details...'
     var url = 'https://webapi.timedoctor.com/v1.1/companies?access_token='+ accessToken +'&_format=json';
-    httpGetAsync(url, function(value){
-        var account = JSON.parse(value).accounts[0];
-        var worklogUrl = 'https://webapi.timedoctor.com/v1.1/companies/'+ account.company_id +'/worklogs?access_token='+ accessToken 
-                         +'&_format=json&start_date='+ startDate +'&end_date='+ endDate +'&user_ids='+ account.user_id;
+    var worklogDates = groupByWeeks(startDate, endDate);
+    const weeks = Object.keys(worklogDates);
+    console.log('group by weeks', worklogDates)
+    console.log('index of weeks', weeks);
+    
+    var getAccount = $.get(url, function(data){
+        var account = data.accounts[0];
+        let weekTotal = {};
+        const getWorklogPerDay = async () => {            
+            for(let i = 0; i < weeks.length; i++){
+                let totalTime = 0;
+                for(let j = 0; j < worklogDates[weeks[i]].length; j++){
+                    let items = await $.get(generateTimeDoctorURL({
+                        company_id: account.company_id,
+                        access_token: accessToken,
+                        start_date: worklogDates[weeks[i]][j].date,
+                        end_date: worklogDates[weeks[i]][j].date,
+                        query_type: 'worklogs',
+                        consolidated: 0
+                    }));
+                    worklogDates[weeks[i]][j].items = items.worklogs.items;
+                    worklogDates[weeks[i]][j].total = items.total;
+                    totalTime += items.total;
+                }
+                console.log('total time', totalTime);
+                weekTotal[weeks[i]] = {};
+                weekTotal[weeks[i]].total = totalTime;
+            }
+            console.log('updated the days', worklogDates);
+            console.log('total time per week', weekTotal);
+        };
+        getWorklogPerDay();
 
-        wageLabel.innerHTML = 'getting tasks and projects...'
-        httpGetAsync(worklogUrl, function(value){
-            var wage = JSON.parse(value).total / 3600 * hourlyRate;
-            console.log(wage);
-            wageLabel.innerHTML = 'Php ' + wage;
+        var paidBreakUrl = generateTimeDoctorURL({
+            company_id: account.company_id,
+            access_token: accessToken,
+            start_date: startDate,
+            end_date: endDate,
+            task_id: 14832166,
+            query_type: 'worklogs',
+            consolidated: 0
         });
+
+        var unconsolodatedLogs = generateTimeDoctorURL({
+            company_id: account.company_id,
+            access_token: accessToken,
+            start_date: startDate,
+            end_date: endDate,
+            query_type: 'worklogs',
+            consolidated: 0
+        });
+
+        var payload = null;
+        $.ajax({
+            async: false,
+            type: 'GET',
+            url: paidBreakUrl,
+            success: function(data) {
+                payload = data.worklogs.items;
+            }
+        });
+        // console.log('payload', payload);
+        var totalPaidBreak = 0;
+        for(var i = 0; i < payload.length; i++){
+            if(payload[i].length <= 900){
+                totalPaidBreak += parseInt(payload[i].length);
+            }
+        }
+        // console.log('total paid break', totalPaidBreak);
+        var workLogs = null;
+        $.ajax({
+            async: false,
+            type: 'GET',
+            url: unconsolodatedLogs,
+            success: function(data) {
+                workLogs = data.worklogs.items;
+            }
+        });
+
+        var totalWorkingTime = 0;
+        for(var i = 0; i < workLogs.length; i++){
+            if(workLogs[i].task_id == 14832166){
+                workLogs.splice(i, 1);
+            } else {
+                totalWorkingTime += parseInt(workLogs[i].length);
+            }
+        }
+        totalWorkingTime += totalPaidBreak;
+        // console.log('total work time', totalWorkingTime);
     });
 });
 
-function httpGetAsync(url, callback){
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function(){
-        if(xmlHttp.readyState == 4 && xmlHttp.status == 200){
-            callback(xmlHttp.responseText);
+var enumerateDaysBetweenDates = function(startDate, endDate) {
+    var now = startDate, dates = [];
+    
+    while (now.isBefore(endDate) || now.isSame(endDate)) {
+        dates.push(now.format('YYYY-MM-DD'));
+        now.add(1, 'days');
+    }
+    return dates;
+};
+
+var groupByWeeks = function(startDate, endDate){
+    var dates = enumerateDaysBetweenDates(moment(startDate), moment(endDate));
+    var groupWeeks = {};
+    //moment().day("Sunday").week(33) generate the first day of a week
+    for(var i = 0; i < dates.length; i++){
+        var index = moment(dates[i]).week();
+        if(groupWeeks[index] instanceof Array){
+            groupWeeks[index].push({date: dates[i]});
+        } else {
+            groupWeeks[index] = [];
+            groupWeeks[index].push({date: dates[i]});
         }
     }
-    xmlHttp.open('GET', url, true);
-    xmlHttp.send(null);
+
+    return groupWeeks;
+}
+
+var generateTimeDoctorURL = function(args){
+    var userId = args.user_id ? '&user_ids=' + args.user_id : '';
+    var consolidated = typeof args.consolidated == 'number' ? '&consolidated=' + args.consolidated : '';
+    var format = args.format ? '&_format=' + args.format : '&_format=json';
+    var taskId = args.task_id ? '&task_ids=' + args.task_id : '';
+
+    var baseURL = 'https://webapi.timedoctor.com/v1.1/companies/';
+    var basicURL = baseURL + args.company_id + '/' + args.query_type + '?access_token=' + args.access_token + format;
+    var queryURL = basicURL + '&start_date=' + args.start_date + '&end_date=' + args.end_date + userId + taskId + consolidated;
+
+    return queryURL;
 }
